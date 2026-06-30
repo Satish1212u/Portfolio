7c9a401cf9b317b222100d5ad36a7a5c1667ea74
Satish Gupta <satishgupta6796@gmail.com>
2026-06-30 09:41:47 +0530

    profile image update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -127,7 +127,7 @@ const Home = () => {
       </section>
 
       <AboutSection />
-      
+
       {/* Featured Projects Section */}
       <section className="py-24 relative overflow-hidden">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
